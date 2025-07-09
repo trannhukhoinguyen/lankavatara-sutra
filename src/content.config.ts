0109bb89ae9b5db42f34f695d246a746b84f9492
Patriarchal Zen <trannhukhoinguyen@gmail.com>
2025-07-09 21:31:35 +0700

    fix bug

--- src/content.config.ts
+++ src/content.config.ts
@@ -4,62 +4,6 @@ import { z, defineCollection } from 'astro:content';
 import { docsSchema } from '@astrojs/starlight/schema';
 import { glob } from 'astro/loaders';
 
-const productsCollection = defineCollection({
-  loader: glob({ pattern: '**/[^_]*.{md,mdx}', base: "./src/content/products" }),
-    schema: ({ image }) => z.object({
-    title: z.string(),
-    description: z.string(),
-    main: z.object({
-      id: z.number(),
-      content: z.string(),
-      imgCard: image(),
-      imgMain: image(),
-      imgAlt: z.string(),
-    }),
-    tabs: z.array(
-      z.object({
-        id: z.string(),
-        dataTab: z.string(),
-        title: z.string(),
-      })
-    ),
-    longDescription: z.object({
-      title: z.string(),
-      subTitle: z.string(),
-      btnTitle: z.string(),
-      btnURL: z.string(),
-    }),
-    descriptionList: z.array(
-      z.object({
-        title: z.string(),
-        subTitle: z.string(),
-      })
-    ),
-    specificationsLeft: z.array(
-      z.object({
-        title: z.string(),
-        subTitle: z.string(),
-      })
-    ),
-    specificationsRight: z.array(
-      z.object({
-        title: z.string(),
-        subTitle: z.string(),
-      })
-    ).optional(),
-    tableData: z.array(
-      z.object({
-        feature: z.array(z.string()),
-        description: z.array(z.array(z.string())),
-      })
-    ).optional(),
-    blueprints: z.object({
-      first: image().optional(),
-      second: image().optional(),
-    }),
-  }),
-});
-
 const sutrasCollection = defineCollection({
   loader: glob({ pattern: '**/[^_]*.{md,mdx}', base: "./src/content/sutras" }),
   schema: ({ image }) => z.object({
@@ -153,7 +97,6 @@ const insightsCollection = defineCollection({
 
 export const collections = {
   docs: defineCollection({ schema: docsSchema() }),
-  'products': productsCollection,
   'sutras': sutrasCollection,
   'blog': blogCollection,
   'insights': insightsCollection,
